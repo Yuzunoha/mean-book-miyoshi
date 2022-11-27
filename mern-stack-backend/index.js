d7a028d7a96ba1dc4f399c29c58c50aa3bd08009
Yuzunoha <studysteadystandby@gmail.com>
2022-11-28 01:26:16 +0900

    return res.status(statusCode).json(msg);

--- mern-stack-backend/index.js
+++ mern-stack-backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  const msg = 'こんにちは!';
+  const statusCode = 200;
+  return res.status(statusCode).json(msg);
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Listening on localhost port ${port}`);
