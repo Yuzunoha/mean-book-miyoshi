ec35cb15100896734f031f6444fa8b5eee8ee73e
Yuzunoha <studysteadystandby@gmail.com>
2022-12-06 22:40:57 +0900

    パスワード認証

--- mern-stack-backend/index.js
+++ mern-stack-backend/index.js
@@ -87,12 +87,15 @@ app.post('/user/login', async (req, res) => {
   try {
     await connectDB();
     const savedUserData = await UserModel.findOne({ email: req.body.email });
-    // TODO
-    console.log({ savedUserData });
-    return res.status(200).json({ message: 'ログイン成功' });
-  } catch (error) {
-    return res.status(400).json({ message: 'ログイン失敗' });
-  }
+    if (savedUserData) {
+      /* ユーザがいる */
+      if (savedUserData.password === req.body.password) {
+        /* パスワードが正しい */
+        return res.status(200).json({ message: 'ログイン成功' });
+      }
+    }
+  } catch (error) {}
+  return res.status(400).json({ message: 'ログイン失敗' });
 });
 
 const port = 5000;
