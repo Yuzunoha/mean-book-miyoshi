b02c5e618aa5430a722331b6317a206b94b2b310
Yuzunoha <studysteadystandby@gmail.com>
2022-12-08 18:41:12 +0900

    作り中

--- mern-stack-backend/utils/auth.js
+++ mern-stack-backend/utils/auth.js
@@ -1,5 +1,14 @@
-const auth = () => {
-  1;
+const auth = async (req, res, next) => {
+  if (req.method === 'GET') {
+    return next();
+  }
+  const token = await req.headers.authorization.split(' ')[1];
+  if (!token) {
+    return res.status(400).json({ message: 'トークンがありません' });
+  }
+  try {
+    // TODO
+  } catch (err) {}
 };
 
 module.exports = auth;
